6471ae69ab6509d6dfc26bfc9c1bbe7bc67bf72e
Rajapandiyan S <155171785+rajapandiyans@users.noreply.github.com>
2025-07-30 18:15:02 +0530

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -4,17 +4,5 @@ export const educations = [
     title: "B.Sc Artificial Intelligence and Machine Learning",
     duration: "2022 - 2025",
     institution: "Rathinam College of Arts and Science",
-  },
-  {
-    id: 2,
-    title: "Higher Secondary Education",
-    duration: "2017 - 2018",
-    institution: "Sri Sankara Vidhyalaya Matric Hr Sec School",
-  },
-  {
-    id: 3,
-    title: "Secondary School Education",
-    duration: "2015 - 2016",
-    institution: "Sri Sankara Vidhyalaya Matric Hr Sec School",
   }
 ]
